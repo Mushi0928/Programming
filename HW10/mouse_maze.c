fd0fd58bbd73399cc404315d8cb4fb6facfe19c1
Ethan Lai <ethanlai0928@gmail.com>
2020-11-28 17:11:33 +0800

    11/28

--- HW10/mouse_maze.c
+++ HW10/mouse_maze.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
+void nextStep(int curR,int curC,int step);
+
 int N,R,C;
-int leastStep = __INT_MAX__,valid = 1;
-int s_Row,s_Col,f_Row,f_Col;
-char maze[505][505];
-int walked[505][505];
+int leastStep = __INT_MAX__;
+int s_Row,s_Col,f_Row,f_Col;    //紀錄S跟F的位置
+char maze[505][505];    
+int walked[505][505];   //紀錄走過的格子
 int main(){
-    scanf("%d",&N);
+    scanf("%d",&N);    
+
     for(int i = 0;i<N;i++){
         scanf("%d %d",&R,&C);
-        for(int j = 0;j<R;j++){
-            scanf("%s",maze[i]);
+        for(int j = 0;j<R;j++){    //scan maze
+            scanf("%s",maze[j]);
         }
-        for(int j = 0;j<R;j++){
+        for(int j = 0;j<R;j++){    //find S and F
             for(int k = 0;k<C;k++){
+                //printf("%c",maze[j][k]);
                 if(maze[j][k] == 'S'){
                     s_Row = j;
                     s_Col = k;
@@ -23,17 +27,62 @@ int main(){
                     f_Col = k;
                 }
             }
+            //printf("\n");
         }
-        nextStep(s_Row,s_Col,0);
-
+        walked[s_Row][s_Col] = 1;    //set S to walked
+        nextStep(s_Row,s_Col,0);    //start recursion
+        walked[s_Row][s_Col] = 0;   //set S back to not-walked
+        //printf("S:%d %d F:%d %d\n",s_Row,s_Col,f_Row,f_Col);
+        printf("%d\n",(leastStep == __INT_MAX__)?-1:leastStep);    //如果leastStep == int_max 代表沒有解 就print -1
+        leastStep = __INT_MAX__;
     }
 
 }
+/*
+    curR,curC:目前的位置
+    step:步數
+    Basis step:
+        如果目前的位置跟F點一樣,就紀錄最小步數,然後return
+    Recursive step:
+        分別嘗試走上下左右四個方向，若不會超出邊界，且下一步的位置沒走過且不為'#'，就進入下個遞迴
+        下個遞迴開始前將下一步的位置的walked設成1，下一步return後再設回來
+        *若當前的步數已經超過(leastStep-1)，表示之後再怎麼跑步數都不會更少了，所以就不做嘗試直接return
+*/
 void nextStep(int curR,int curC,int step){
+    printf("curR:%4d curC:%d step:%4d\n",curR,curC,step);
     if(curR == f_Row && curC == f_Col){
         if(step < leastStep){
             leastStep = step;
         }
-        return;
+    }else if(step<leastStep-1){
+        if(curR > 0){
+            if(maze[curR-1][curC] != '#' && !walked[curR-1][curC]){
+                walked[curR-1][curC] = 1;
+                nextStep(curR-1,curC,step+1);
+                walked[curR-1][curC] = 0;
+            }
+        }
+        if(curR < R-1){
+            if(maze[curR+1][curC] != '#' && !walked[curR+1][curC]){
+                walked[curR+1][curC] = 1;
+                nextStep(curR+1,curC,step+1);
+                walked[curR+1][curC] = 0;
+            }
+        }
+        if(curC > 0){
+            if(maze[curR][curC-1] != '#' && !walked[curR][curC-1]){
+                walked[curR][curC-1] = 1;
+                nextStep(curR,curC-1,step+1);
+                walked[curR][curC-1] = 0;
+            }
+        }
+        if(curC < C-1){
+            if(maze[curR][curC+1] != '#' && !walked[curR][curC+1]){
+                walked[curR][curC+1] = 1;
+                nextStep(curR,curC+1,step+1);
+                walked[curR][curC+1] = 0;
+            }
+        }
     }
+
 }
